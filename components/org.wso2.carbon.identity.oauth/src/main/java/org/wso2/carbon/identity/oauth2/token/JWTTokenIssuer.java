49fd094b99b69aaf66b0ec6dda6ba7306a676711
vivekvinushanth <cvivekvinushanth@gmail.com>
2024-01-05 13:52:27 +0530

    add binding value to cnf

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/token/JWTTokenIssuer.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/token/JWTTokenIssuer.java
@@ -825,7 +825,8 @@ public class JWTTokenIssuer extends OauthTokenIssuerImpl {
             String bindingType = tokReqMsgCtx.getTokenBinding().getBindingType();
             jwtClaimsSetBuilder.claim(TOKEN_BINDING_REF, tokReqMsgCtx.getTokenBinding().getBindingReference());
             jwtClaimsSetBuilder.claim(TOKEN_BINDING_TYPE, bindingType);
-            if (OAuth2Constants.TokenBinderType.CERTIFICATE_BASED_TOKEN_BINDER.equals(bindingType)) {
+            if (OAuth2Constants.TokenBinderType.CERTIFICATE_BASED_TOKEN_BINDER.equals(bindingType) ||
+                    OAuth2Constants.TokenBinderType.CLIENT_REQUEST.equals(bindingType)) {
                 String cnf = tokReqMsgCtx.getTokenBinding().getBindingValue();
                 if (StringUtils.isNotBlank(cnf)) {
                     jwtClaimsSetBuilder.claim(OAuthConstants.CNF, Collections.singletonMap(OAuthConstants.X5T_S256,
